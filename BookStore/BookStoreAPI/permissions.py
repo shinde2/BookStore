3ddbf6f8ab3330354ed3d37bc817a30e516de1ec
shinde2 <ahs278@cornell.edu>
2023-05-10 16:26:12 -0700

    Adding custom permissions

---
+++ BookStore/BookStoreAPI/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsManager(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Manager")
+
+
+class IsDeliveryCrew(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="Delivery Crew")
