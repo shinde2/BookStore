4eb79804649aa82d89305ad84b3c00e5dd5ea780
shinde2 <ahs278@cornell.edu>
2023-05-10 18:17:10 -0700

    Adding app urls

---
+++ BookStore/BookStoreAPI/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from BookStoreAPI import views
+
+
+urlpatterns = [
+    path('books', views.BookItemsList.as_view()),
+    path('books/<int:pk>', views.BookItemsDetail.as_view()),
+    path('orders', views.OrdersList.as_view()),
+    path('orders/<int:pk>', views.OrdersDetail.as_view()),
+    path('cart/books', views.Carts.as_view()),
+    path('groups/manager/users', views.ManagersList.as_view()),
+    path('groups/manager/users/<int:pk>', views.ManagersDetail.as_view()),
+    path('groups/carrier/users', views.CarrierList.as_view()),
+    path('groups/carrier/users/<int:pk>', views.CarrierDetail.as_view()),
+]
