d41b403b624cfd67ed8ab3a567a1d457dcd232c8
shinde2 <ahs278@cornell.edu>
2023-05-18 18:27:05 -0700

    Adding test settings

--- BookStore/manage.py
+++ BookStore/manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookStore.settings')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookStore.settings')
+    if len(sys.argv) > 1 and sys.argv[1] == "test":
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookStore.test_settings')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookStore.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
